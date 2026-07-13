2ba0fa560a26efd472be7a3487e98b6768cac20c
liyishi <1252651434@qq.com>
2026-07-13 12:34:50 +0800

    add log
    Signed-off-by: liyishi <1252651434@qq.com>

--- vllm_ascend/attention/mla_v1.py
+++ vllm_ascend/attention/mla_v1.py
@@ -1067,6 +1067,7 @@ class AscendMLAImpl(MLAAttentionImpl):
     ):
         assert len(kv_c_and_k_pe_cache) > 1
         prefill_metadata = attn_metadata.prefill
+        logger.info(f"[lys] prefill_metadata.chunked_context: {prefill_metadata.chunked_context}")
         if prefill_metadata is None or prefill_metadata.chunked_context is None:
             return prefix_output, prefix_lse
 
